e63be966a527f62d67629810e09393a8d54613bb
Chris Chalfant <cchalfant@leafsoftwaresolutions.com>
2014-07-07 10:34:24 -0400

    tests and cosmetic changes to ec2/image.rb

--- lib/automat/ec2/image.rb
+++ lib/automat/ec2/image.rb
@@ -7,6 +7,17 @@ module Automat::Ec2
 
     include Automat::Mixins::Utils
 
+    def initialize(options=nil)
+      super
+      @wait = Wait.new({
+        delay: 5,
+        attempts: 24, # 24 x 5s == 2m
+        debug: true,
+        rescuer:  WaitRescuer.new,
+        logger:   @logger
+      })
+    end
+
     def find_inst
       inst = nil
       if !instance.nil?
@@ -19,85 +30,99 @@ module Automat::Ec2
       inst = find_inst
       myname = default_image_name
       logger.info "Creating image #{myname} for #{inst.id}"
-      newami=ec2.images.create(:instance_id=>instance,:name=>myname,:no_reboot=>true)
+      newami = ec2.images.create(instance_id: instance, name: myname, no_reboot: true)
       if prune == true
         set_prunable(newami)
       end
     end
 
     def is_more_than_month_old?(mytime)
-      if mytime.class == Time && mytime<Time.now.utc-(60*60*24*30)
+      if mytime.class == Time && mytime < Time.now.utc - (60*60*24*30)
         true
       else
         false
       end
     end
 
-    def default_image_name()
-      stime = Time.new.iso8601.gsub(/:/,'-')
+    def default_image_name
+      stime = Time.new.iso8601.gsub(/:/, '-')
       return name + "-" + stime
     end
 
-    def check_for_ebs(block_devices)
-      block_devices.each do |device|
-        if (!device.nil? && !device[:ebs].nil? && !device[:ebs][:snapshot_id].nil?)
-          return true
+    def image_snapshot_exists?(image)
+      !image_snapshot(image).nil?
+    end
+
+    def image_snapshot(image)
+      image.block_devices.each do |device|
+        if !device.nil? &&
+           !device[:ebs].nil? &&
+           !device[:ebs][:snapshot_id].nil?
+
+          return device[:ebs][:snapshot_id]
         end
       end
-      return false
+      nil
     end
 
     def set_prunable(newami)
-      logger.info "setting prunable for AMI #{newami.image_id}"
-      newami.tags["CanPrune"]="yes"
-      until check_for_ebs(newami.block_devices)
-        logger.info "waiting 5sec for a valid snapshot so that we can tag it"
-        sleep 5
+      logger.info "Setting prunable for AMI #{newami.image_id}"
+      newami.tags["CanPrune"] = "yes"
+
+      wait.until do
+        logger.info "Waiting for a valid snapshot so we can tag it."
+        image_snapshot_exists? newami
       end
-      newami.block_devices.each do |device|
-        if device[:ebs]
-          logger.info "setting prunable for snapshot #{ec2.snapshots[device[:ebs][:snapshot_id]].id}"
-          ec2.snapshots[device[:ebs][:snapshot_id]].tags["CanPrune"]="yes"
-        end
-      end
+
+      snapshot = image_snapshot(newami)
+      logger.info "Setting prunable for snapshot #{snapshot}"
+      ec2.snapshots[snapshot].tags["CanPrune"] = "yes"
     end
 
     def deregister_images(snaplist)
-      my_images=ec2.images.with_owner('self')
+      my_images = ec2.images.with_owner('self')
       my_images.each do |oneimage|
-        next unless oneimage.state == :available
-        if oneimage.tags["CanPrune"]=="yes" and snaplist.include?(oneimage.block_devices[0][:ebs][:snapshot_id])
-          logger.info "deregistering AMI #{oneimage.id}"
+        if oneimage.state != :available
+          logger.warn "AMI #{oneimage.id} could not be deleted because its state is #{oneimage.state}"
+          next
+        end
+
+        if oneimage.tags["CanPrune"] == "yes" and snaplist.include?(oneimage.block_devices[0][:ebs][:snapshot_id])
+          logger.info "Deregistering AMI #{oneimage.id}"
           oneimage.delete
         end
       end
     end
 
     def delete_snapshots(snaplist)
-      allsnapshots=ec2.snapshots.with_owner('self')
-      allsnapshots.each do |onesnapshot|
-        next unless onesnapshot.status == :completed
-        if snaplist.include?(onesnapshot.id)
-          logger.info "deleting snapshot #{onesnapshot.id}"
-          onesnapshot.delete
+      snaplist.each do |snap|
+        if snap.status != :completed
+          logger.warn "Snapshot #{snap.id} could not be deleted because its status is #{snap.status}"
+          next
         end
+
+        logger.info "Deleting snapshot #{snap.id}"
+        snap.delete
       end
+
     end
 
-    def prune_amis()
-      condemnedsnaps=[]
-      allsnapshots=ec2.snapshots.with_owner('self')
+    def prune_amis
+      condemned_snaps = []
+      allsnapshots = ec2.snapshots.with_owner('self')
       allsnapshots.each do |onesnapshot|
         next unless onesnapshot.status == :completed
-        mycreatetime=onesnapshot.start_time
-        if is_more_than_month_old?(mycreatetime) and onesnapshot.tags["CanPrune"]=="yes"
-          logger.info "adding snapshot #{onesnapshot.id} to condemed list"
-          condemnedsnaps.push(onesnapshot.id)
+
+        mycreatetime = onesnapshot.start_time
+        if is_more_than_month_old?(mycreatetime) and onesnapshot.tags["CanPrune"] == "yes"
+          logger.info "Adding snapshot #{onesnapshot.id} to condemed list"
+          condemned_snaps.push onesnapshot.id
         end
       end
-      if !condemnedsnaps.empty?
-        deregister_images(condemnedsnaps)
-        delete_snapshots(condemnedsnaps)
+
+      unless condemned_snaps.empty?
+        deregister_images condemned_snaps
+        delete_snapshots  condemned_snaps
       end
     end
 
--- spec/ec2/image_spec.rb
+++ spec/ec2/image_spec.rb
@@ -6,6 +6,8 @@ describe Automat::Ec2::Image do
   it { should respond_to :create }
   it { should respond_to :prune }
   it { should respond_to :default_image_name }
+  it { should respond_to :image_snapshot_exists? }
+  it { should respond_to :image_snapshot }
 
   describe '#default_image_name' do
     subject(:s) do
@@ -25,5 +27,38 @@ describe Automat::Ec2::Image do
       name.should match /^skee-lo-(\d{4})-(\d{2})-(\d{2})T(\d{2})-(\d{2})-(\d{2})[+-](\d{2})-(\d{2})/
     end
 
+  end
+
+  describe '#image_snapshot' do
+    subject(:s) do
+      AWS.stub!
+      s = Automat::Ec2::Image.new
+      s.logger = Logger.new('/dev/null')
+      s
+    end
+
+    it "returns id if the snapshot exists" do
+      fake_ami = double("ami")
+      fake_ami.stub(:block_devices) { [ { ebs: {snapshot_id: 'foo' } } ] }
+      s.image_snapshot(fake_ami).should eq('foo')
+    end
+
+    it "returns nil if the snapshot is nil" do
+      fake_ami = double("ami")
+      fake_ami.stub(:block_devices) { [ { ebs: {snapshot_id: nil } } ] }
+      s.image_snapshot(fake_ami).should be_nil
+    end
+
+    it "returns nil if the ebs is nil" do
+      fake_ami = double("ami")
+      fake_ami.stub(:block_devices) { [ { ebs: nil } ] }
+      s.image_snapshot(fake_ami).should be_nil
+    end
+
+    it "returns nil if the device is nil" do
+      fake_ami = double("ami")
+      fake_ami.stub(:block_devices) { [ nil ] }
+      s.image_snapshot(fake_ami).should be_nil
+    end
   end
 end
